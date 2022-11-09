d779dae61e8824fba5794df72eaf3dc4c162820d
Vipin <vipinrwt9@gmail.com>
2022-11-09 20:20:55 +0530

    Add files via upload
    ADDED FROM DESKTOP

---
+++ App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from './components/ExpenseItem';
+import ListExpenses from './components/ListExpenses';
+function App() {
+  return (
+    <div>
+      <h1>Let's get started</h1>
+      <ExpenseItem></ExpenseItem>
+      <ListExpenses></ListExpenses>
+    </div>
+  );
+}
+
+export default App;
---
+++ ListExpenses.js
@@ -0,0 +1,8 @@
+function ListExpenses()
+{
+  return <p>Food      Rs10<br></br>
+             Petrol    Rs100<br></br>
+             Movies    Rs200<br></br>
+          </p>
+}
+export default ListExpenses;
